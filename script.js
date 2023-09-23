3273a2e65e940601abf758c2aa3fee9752c16300
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-23 11:51:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const text = document.querySelector('h1')
+
+window.addEventListener('resize',updateText)
+
+function updateText(){
+  text.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+}
+
+updateText()
